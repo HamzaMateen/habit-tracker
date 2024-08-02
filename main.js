bedc48eae315fb8cbd421ac4ad1760b349253ebf
HamzaMateen <hamzamateen1583@gmail.com>
2024-08-02 22:57:50 +0500

    aug 2

---
+++ main.js
@@ -0,0 +1,27 @@
+// Add squares
+const squares = document.querySelector('.squares');
+
+const DAY_FAILED = 4;
+const DID_SOMETHING_EXTRA = 3;
+const BOTH_GOALS_ACHIEVED = 2;
+const BARE_MINIMUM = 1;
+const PREV_MONTH = 0;
+
+// first three days from the July
+for (let i = 0; i < 3; i++) {
+  squares.insertAdjacentHTML('beforeend', `<li data-level="${PREV_MONTH}"></li>`);
+}
+
+// Empty additions till Friday (exclusive)
+const progressUpdate = (achievement) => {
+  squares.insertAdjacentHTML('beforeend', `<li data-level="${achievement}"></li>`);
+}
+
+// starting from August
+progressUpdate(DAY_FAILED);
+progressUpdate(DAY_FAILED);
+progressUpdate(BARE_MINIMUM);
+
+
+
+
